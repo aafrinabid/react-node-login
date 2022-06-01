76f96dcd7882aa5205820bed5fe817176bbd16e1
aafrinabid <mohdaafrin@gmail.com>
2022-06-01 17:32:36 +0530

    before jwt

---
+++ server/app.js
@@ -0,0 +1,85 @@
+const express=require('express');
+const app=express()
+const pool=require('./db');
+const bcrypt=require('bcrypt');
+const cors=require('cors');
+const jwt=require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+const session=require('express-session')
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(cors())
+app.use(cookieParser());
+// app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(
+    session({
+        key:'userId',
+        secret:'nicework',
+        resave:false,
+        saveUninitialized:false,
+        cookie:{
+            expires:60*60*24,
+        },
+    })
+);
+
+
+
+app.post('/register',async(req,res)=>{
+    try{
+        const {username,password,email}=req.body
+        const user= await pool.query('SELECT * FROM users WHERE username=$1',[username]);
+        console.log(user)
+        if(user.rowCount>0){
+          throw new Error(`person with this username already exist`)
+        }else{
+            const useremail=await pool.query('SELECT * FROM users WHERE email=$1',
+            [email])
+            if(useremail.rowCount>0){
+                throw new Error(' email already exist')
+            }
+            const hashedPassword=await bcrypt.hash(password,10);
+            const NewUser=await pool.query('INSERT INTO users(username,password,email,roles) VALUES($1,$2,$3,$4) RETURNING *',
+            [username,hashedPassword,email,'user']);
+            console.log(NewUser.rows[0])
+            res.status(200).json(NewUser.rows[0])
+        }
+        
+    }
+    catch(err){
+        res.status(400).json(err.message)
+    }
+})
+
+app.post('/login',async(req,res)=>{
+    try{
+        let loginStatus=false
+        const {username,password}=req.body;
+        const user= await pool.query('SELECT * FROM users WHERE username=$1',[username]);
+        console.log(user);
+        if(user.rowCount===1){
+            const compPass=await bcrypt.compare(password,user.rows[0].password)
+            if(compPass){
+                 loginStatus=true
+                 req.session.user=user.rows[0];
+                // res.json([user,loginStatus]);
+                res.status(200).json(user.rows[0])
+            }else{
+                throw new Error('password is wrong')
+            }
+        //   return res.send(`person with this username already exist`)
+        }else{
+throw new Error('user doesnot exist please register')
+        }
+        
+    }
+    catch(err){
+        res.status(400).json(err.message)
+    }
+})
+
+app.listen(4000,()=>{
+    console.log('listening at 4000');
+});
---
+++ server/db.js
@@ -0,0 +1,18 @@
+(async function main(){
+    const pg=require('pg')
+    const connectionString='postgressql://postgres:newPassword@localhost:5432/login'
+    var client = new pg.Client(connectionString);
+    client.connect(function(err) {
+      if(err) {
+        return console.error('could not connect to postgres', err);
+      }
+      client.query('SELECT NOW() AS "theTime"', function(err, result) {
+        if(err) {
+          return console.error('error running query', err);
+        }
+        console.log(result.rows[0].theTime);
+        // >> output: 2018-08-23T14:02:57.117Z
+      });
+    });
+    module.exports=client
+    })()
